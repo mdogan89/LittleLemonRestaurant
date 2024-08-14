b406f393870f01b1450cd4fad36cf97c07d59e7b
Mustafa Dogan <mdogan89@hotmail.com>
2024-08-15 01:59:29 +0300

    run npm build

--- little-lemon-app/src/components/Specials.js
+++ little-lemon-app/src/components/Specials.js
@@ -1,30 +1,30 @@
 import React, { useState } from 'react'
 import Card from './Card'
-import svg from "../img/bruchetta.svg"
+// import svg from "../img/bruchetta.jpg"
 
 import axios from 'axios';
 import { useEffect } from 'react';
 
-const specials = [
-  {
-    title: "Greek Salad",
-    price: "$ 12.99",
-    description: "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.",
-    getImageSrc: () => require("../img/greek salad.jpg")
-  },
-  {
-    title: "Bruschetta",
-    price: "$ 5.99",
-    description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
-    getImageSrc: () => svg
-  },
-  {
-    title: "Lemon Dessert",
-    price: "$ 5.00",
-    description: "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
-    getImageSrc: () => require("../img/lemon dessert.jpg")
-  }
-]
+// const specials = [
+//   {
+//     title: "Greek Salad",
+//     price: "$ 12.99",
+//     description: "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.",
+//     getImageSrc: () => require("../img/greek salad.jpg")
+//   },
+//   {
+//     title: "Bruschetta",
+//     price: "$ 5.99",
+//     description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+//     getImageSrc: () => svg
+//   },
+//   {
+//     title: "Lemon Dessert",
+//     price: "$ 5.00",
+//     description: "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+//     getImageSrc: () => require("../img/lemon dessert.jpg")
+//   }
+// ]
 
 
 

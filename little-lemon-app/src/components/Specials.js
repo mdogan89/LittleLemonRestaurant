9a4b67d9d78b9527021fc87ecddfff16bebd209c
Mustafa Dogan <mdogan89@hotmail.com>
2024-08-16 00:25:12 +0300

    changed http urls to https

--- little-lemon-app/src/components/Specials.js
+++ little-lemon-app/src/components/Specials.js
@@ -16,7 +16,7 @@ export default function Specials() {
 
   useEffect(() => {
     // invalid url will trigger an 404 error
-    axios.get(`http://littlelemonrestaurant-mdogan89s-projects.vercel.app/restaurant/menu/`).then((response) => {
+    axios.get(`https://littlelemonrestaurant-mdogan89s-projects.vercel.app/restaurant/menu/`).then((response) => {
       const menuItems = response.data
       setMenuItems(menuItems)
       console.log(menuItems);

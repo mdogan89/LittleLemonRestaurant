9750e5d689175228ea940e4da7ed1a5a74db9c19
Mustafa Dogan <mdogan89@hotmail.com>
2024-08-15 19:58:39 +0300

    changed vercel.json

--- little-lemon-app/src/components/Specials.js
+++ little-lemon-app/src/components/Specials.js
@@ -1,30 +1,10 @@
 import React, { useState } from 'react'
 import Card from './Card'
-// import svg from "../img/bruchetta.jpg"
 
 import axios from 'axios';
 import { useEffect } from 'react';
 
-// const specials = [
-//   {
-//     title: "Greek Salad",
-//     price: "$ 12.99",
-//     description: "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.",
-//     getImageSrc: () => require("../img/greek salad.jpg")
-//   },
-//   {
-//     title: "Bruschetta",
-//     price: "$ 5.99",
-//     description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
-//     getImageSrc: () => svg
-//   },
-//   {
-//     title: "Lemon Dessert",
-//     price: "$ 5.00",
-//     description: "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
-//     getImageSrc: () => require("../img/lemon dessert.jpg")
-//   }
-// ]
+
 
 
 
@@ -36,7 +16,7 @@ export default function Specials() {
 
   useEffect(() => {
     // invalid url will trigger an 404 error
-    axios.get(`http://localhost:8000/restaurant/menu/`).then((response) => {
+    axios.get(`http://littlelemonrestaurant-mdogan89s-projects.vercel.app/restaurant/menu/`).then((response) => {
       const menuItems = response.data
       setMenuItems(menuItems)
       console.log(menuItems);
